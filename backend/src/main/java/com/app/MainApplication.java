b1ed9fc3907e9950471e97a5b0151a5449352d29
yashverma03 <vermayash2003@gmail.com>
2025-12-07 12:37:08 +0530

    feat: Update server startup logic to dynamically retrieve and display the server port, enhancing startup feedback

--- backend/src/main/java/com/app/MainApplication.java
+++ backend/src/main/java/com/app/MainApplication.java
@@ -2,13 +2,16 @@ package com.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 public class MainApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MainApplication.class, args);
-		System.out.println("\u001B[32mServer started on port 3000\u001B[0m");
+		var context = SpringApplication.run(MainApplication.class, args);
+		Environment env = context.getEnvironment();
+		String port = env.getProperty("server.port");
+		System.out.println("\u001B[32mServer started on port " + port + "\u001B[0m");
 	}
 
 }
